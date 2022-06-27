11124ffda558f461880040798c3ca1a03e1a2e08
daon <105149013+leedaon@users.noreply.github.com>
2022-06-27 11:50:29 +0900

    MemberController 에 로그인 처리를 해주는 메소드 만듬
    MemberController 에 loginMember 메소드 만듬 - 다온

--- Test_Project/src/main/java/com/kh/test/member/controller/MemberController.java
+++ Test_Project/src/main/java/com/kh/test/member/controller/MemberController.java
@@ -1,5 +1,25 @@
 package com.kh.test.member.controller;
 
-public class MemberController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.test.member.model.vo.Member;
+
+@Controller
+public class MemberController {
+	
+	/** alt + shift + j
+	 * 
+	 * 로그인 처리를 해주는 메소드 - 다온
+	 * @param m => 로그인할 사용자의 계정 (아이디, 비번)
+	 * @return => 메인페이지로 url 재요청
+	 */
+	
+	// 협업할때 주석 필수
+	@RequestMapping("login.me")
+	public String loginMember(Member m) {
+		return "redirect:/";
+	}
+
+	
 }
